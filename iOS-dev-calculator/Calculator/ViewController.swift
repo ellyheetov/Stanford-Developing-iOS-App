c43139d3212eeefcd8d4ffc139e9311bc8cf63b6
ellyheetov <yaaajcm@naver.com>
2020-12-15 14:53:12 +0900

    feat(calculator) : Navigation Controller 추가
    메모리를 추적할 수 있는 전역 변수 추가

--- iOS-dev-calculator/Calculator/ViewController.swift
+++ iOS-dev-calculator/Calculator/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+var calculatorCount = 0
 class ViewController: UIViewController {
     
     @IBOutlet private weak var display: UILabel!
@@ -15,6 +16,15 @@ class ViewController: UIViewController {
     private var userIsInTheMiddleOftyping = false
     private var brain = CalculatorBrain()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculatorCount += 1
+        print("Loaded up a new Calculator (Count = \(calculatorCount))")
+    }
+    deinit {
+        calculatorCount -= 1
+        print("Calculator left the heap (Count = \(calculatorCount))")
+    }
     // property가 저장 뿐만아니라 연산도 가능하다.
     var displayValue : Double {
         /* display의 text는 Optional String 타입이다. 매번 연산을 하기 위해 display.text를 강제 형변환 하는코드가 필요하다. 반복되는 코드를 작성하는 것을 피하기 위하여 변수로 지저하였다. get을 이용하여 Double형으로 변환된 값을 얻어 올 수 있으며, set을 통하여 새로운 값을 display.text로 지정할 할 수 있다.
