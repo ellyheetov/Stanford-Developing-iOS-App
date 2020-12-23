17351d56259166a9a528eb2d8d9cf6e98cde3c1f
ellyheetov <yaaajcm@naver.com>
2020-12-23 20:47:14 +0900

    add(playingcard): PlayCardView에 대한 비율을 상수로 선언
    구조체 extension

--- iOS-dev-playingCard/playingCard/PlayingCardView.swift
+++ iOS-dev-playingCard/playingCard/PlayingCardView.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class PlayingCardView: UIView {
 
+    var rank: Int = 6
+    var suit: String = "♥️"
+    var isFaceUp: Bool = true
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -17,4 +20,32 @@ class PlayingCardView: UIView {
     }
     */
 
+}
+
+extension PlayingCardView {
+    private struct SizeRatio {
+        static let cornerFontSizeToBoundsHeight: CGFloat = 0.085
+        static let cornerRadiusToBoundsHeight: CGFloat = 0.06
+        static let cornerOffsetToCornerRadius: CGFloat = 0.33
+        static let faceCardImageSizeToBoundsSize: CGFloat = 0.75
+    }
+    private var cornerRadius: CGFloat {
+        return bounds.size.height * SizeRatio.cornerRadiusToBoundsHeight
+    }
+    private var cornerOffset: CGFloat {
+        return cornerRadius * SizeRatio.cornerOffsetToCornerRadius
+    }
+    private var cornerFontSize: CGFloat {
+        return bounds.size.height * SizeRatio.cornerFontSizeToBoundsHeight
+    }
+    private var rankString: String {
+        switch rank {
+        case 1: return "A"
+        case 2...10: return String(rank)
+        case 11: return "J"
+        case 12: return "Q"
+        case 13: return "K"
+        default: return "?"
+        }
+    }
 }
