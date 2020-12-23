b9f55b1637dccd46a7e04f3a8c7dd1e0b38a52fe
ellyheetov <yaaajcm@naver.com>
2020-12-23 20:40:07 +0900

    add(playingcard): Playing Card View 생성

---
+++ iOS-dev-playingCard/playingCard/PlayingCardView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayingCardView.swift
+//  playingCard
+//
+//  Created by 박혜원 on 2020/12/23.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
